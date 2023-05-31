c2325512a8720f4c406bf597346b329aed608fc7
ashiqsultan <ashiqdrive@gmail.com>
2023-05-31 15:21:33 +0530

    Initiated MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,16 @@
-import { listDocuments } from './api/api';
-import { Databases, ID } from 'appwrite';
-import { createAccount, login, getInfo, logout } from './api/account';
-import { create, getAll } from './api/posts';
-import { getByStateId } from './api/cities';
-function App() {
-  const onClick = async () => {
-    console.log(await listDocuments());
-  };
-  const onClickCreateAccount = async () => {
-    // const account = await createAccount('ashiq02@example.com', 'thisisapwd');
-  };
-  const onClickLogin = async () => {
-    console.log(await login('ashiq02@example.com', 'thisisapwd'));
-  };
-  const onClickInfo = async () => {
-    const accountInfo = await getInfo();
-    console.log(accountInfo);
-  };
-  const onClicklogout = async () => {
-    const res = await logout();
-    console.log(res);
-  };
-  const onCreate = async () => {
-    const res = await create('', '', 1, '', '', '');
-    console.log(res);
-  };
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import AppBar from './components/AppBar';
 
+function App() {
   return (
-    <div>
-      Sell Smart
-      <div>
-        <button onClick={onClick}>Test API</button>
-      </div>
-      <div>
-        <button onClick={onClickLogin}>Login</button>
-      </div>
-      <div>
-        <button onClick={onClickInfo}>Get Info</button>
-      </div>
-      <div>
-        <button onClick={onClicklogout}>Logout</button>
-      </div>
-      <div>
-        <button onClick={getAll}>List Posts</button>
-      </div>
-      <div>
-        <button onClick={onCreate}>Create Posts</button>
-      </div>
-    </div>
+    <Router>
+      <AppBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<div>404 Page Not Found</div>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+import { Typography } from '@mui/material';
+
+function Home() {
+  return <Typography>This is Home Page</Typography>;
+}
+
+export default Home;
