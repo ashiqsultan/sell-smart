f2a671a0abf6028dde53c8ae9cb4a0b9e0865952
ashiqsultan <ashiqdrive@gmail.com>
2023-05-31 15:58:52 +0530

    Added Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Home from './components/Home';
 import AppBar from './components/AppBar';
+import Login from './components/Login';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#008060',
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <AppBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='*' element={<div>404 Page Not Found</div>} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AppBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<div>404 Page Not Found</div>} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Login.tsx
@@ -0,0 +1,52 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../api/account';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const handleSubmit = async (event: any) => {
+    try {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      const email = data.get('email') as string;
+      const password = data.get('password') as string;
+      const isLoginRequest = await login(email, password);
+      if (isLoginRequest.$id) {
+        navigate('/');
+      }
+    } catch (error) {
+      console.error('Login Error');
+    }
+  };
+
+  return (
+    <Box component='form' onSubmit={handleSubmit} noValidate>
+      <TextField
+        margin='normal'
+        required
+        fullWidth
+        id='email'
+        label='Email Address'
+        name='email'
+        autoComplete='email'
+        autoFocus
+      />
+      <TextField
+        margin='normal'
+        required
+        fullWidth
+        name='password'
+        label='Password'
+        type='password'
+        id='password'
+        autoComplete='current-password'
+      />
+      <Button type='submit' fullWidth variant='contained' sx={{ mt: 2, mb: 2 }}>
+        Sign In
+      </Button>
+    </Box>
+  );
+};
+export default Login;
