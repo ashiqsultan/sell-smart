3d24277d148a9308b8a5beda33c158ce93c8156d
ashiqsultan <ashiqdrive@gmail.com>
2023-06-03 00:53:58 +0530

    Moved Ciy selector states to context

--- src/components/Selectors/CitySelector.tsx
+++ src/components/Selectors/CitySelector.tsx
@@ -1,44 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 import { getByStateId, ICity } from '../../api/cities';
+import { AppContext } from '../../context/AppContext';
 
-interface CitySelectorProps {
-  stateId: string;
-  onChange: (cityId: string) => void;
-}
-
-const CitySelector: React.FC<CitySelectorProps> = ({ stateId, onChange }) => {
-  const [cities, setCities] = useState<ICity[]>([]);
-  const [selectedCity, setSelectedCity] = useState<string>('');
-
-  useEffect(() => {
-    const fetchCities = async () => {
-      try {
-        const cityList = await getByStateId(stateId);
-        setCities(cityList);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    if (stateId) {
-      fetchCities();
-    } else {
-      setCities([]); // Reset cities if stateId is not provided
-    }
-  }, [stateId]);
+const CitySelector: React.FC = () => {
+  const { state, dispatch } = useContext(AppContext);
 
   const handleCityChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     const cityId = event.target.value as string;
-    setSelectedCity(cityId);
-    onChange(cityId); // Pass the selected city $id back to the parent component
+    dispatch({ type: 'SET_CITY_ID', payload: cityId });
   };
 
   return (
     <FormControl fullWidth>
       <InputLabel>City</InputLabel>
-      <Select value={selectedCity} onChange={handleCityChange}>
-        {cities.map((city) => (
+      <Select value={state.cityId} onChange={handleCityChange}>
+        {state.cities.map((city) => (
           <MenuItem key={city.$id} value={city.$id}>
             {city.name}
           </MenuItem>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import PostList from '../components/PostList/PostList';
 import StateSelector from '../components/Selectors/StateSelector';
+import CitySelector from '../components/Selectors/CitySelector';
 
 function Home() {
   return (
     <>
       <StateSelector />
+      <CitySelector />
       <PostList />
     </>
   );
