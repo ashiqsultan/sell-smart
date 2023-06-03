e2203653f6e7ed77511908261a6e3627e6e8162c
ashiqsultan <ashiqdrive@gmail.com>
2023-06-03 16:55:54 +0530

    Changed Home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,17 +3,32 @@ import StateSelector from '../components/Selectors/StateSelector';
 import CitySelector from '../components/Selectors/CitySelector';
 import Category from '../components/Selectors/Category';
 import PriceMinMaxField from '../components/PriceRangeTextField';
+import { Grid, Card, CardContent } from '@mui/material';
 
 function Home() {
   return (
-    <>
-      <Category />
-      <StateSelector />
-      <CitySelector />
-      <PriceMinMaxField isMinPrice={true} />
-      <PriceMinMaxField isMinPrice={false} />
-      <PostList />
-    </>
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={4}>
+        <Card>
+          <CardContent>
+            <Category />
+            <StateSelector />
+            <CitySelector />
+            <Grid container>
+              <Grid item xs={6}>
+                <PriceMinMaxField isMinPrice={true} />
+              </Grid>
+              <Grid item xs={6}>
+                <PriceMinMaxField isMinPrice={false} />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <PostList />
+      </Grid>
+    </Grid>
   );
 }
 
