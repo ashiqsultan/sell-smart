be3b9c0dff65cb7899bf344c9bac049d813167e7
ashiqsultan <ashiqdrive@gmail.com>
2023-06-07 00:21:42 +0530

    Updated Post Details

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -3,6 +3,9 @@ import { Box, Typography, Divider, Grid } from '@mui/material';
 import { IPost, getById } from '../api/posts';
 import { useParams } from 'react-router-dom';
 import PostImageViewer from './PostImageViewer';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import RoomIcon from '@mui/icons-material/Room';
 
 const PostDetails: React.FC = () => {
   const { postId } = useParams();
@@ -47,40 +50,55 @@ const PostDetails: React.FC = () => {
         <Grid item xs={12} sm={6}>
           <PostImageViewer imageIds={image_ids || []} />
         </Grid>
-        <Grid item xs={12} sm={6} container spacing={2} mt={3}>
-          <Grid item xs={6}>
-            <Typography variant='subtitle1'>User ID:</Typography>
-            <Typography>{user_id}</Typography>
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          container
+          spacing={1}
+          style={{ marginBottom: '20px' }}
+        >
+          <Grid item xs={12}>
+            <Typography variant='h4'>{title}</Typography>
+            <Typography>
+              <span style={{ fontSize: '18px' }}>₹</span>{' '}
+              <span
+                style={{ color: 'green', fontSize: '20px', fontWeight: 'bold' }}
+              >
+                {price}
+              </span>
+            </Typography>
           </Grid>
-
-          <Grid item xs={6}>
-            <Typography variant='subtitle1'>Category ID:</Typography>
-            <Typography>{category_id}</Typography>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Typography variant='subtitle1'>Title:</Typography>
-            <Typography>{title}</Typography>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Typography variant='subtitle1'>Price:</Typography>
-            <Typography>{price}</Typography>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Typography variant='subtitle1'>State ID:</Typography>
-            <Typography>{state_id}</Typography>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Typography variant='subtitle1'>City ID:</Typography>
-            <Typography>{city_id}</Typography>
+          <Grid item xs={12} style={{ textAlign: 'right' }}>
+            <Grid container alignItems='center' spacing={1}>
+              <Grid item>
+                <AccountCircleIcon />
+              </Grid>
+              <Grid item>
+                <Typography>{user_id}</Typography>
+              </Grid>
+            </Grid>
+            <Grid container alignItems='center' spacing={1}>
+              <Grid item>
+                <LocationCityIcon />
+              </Grid>
+              <Grid item>
+                <Typography>{state_id}</Typography>
+              </Grid>
+            </Grid>
+            <Grid container alignItems='center' spacing={1}>
+              <Grid item>
+                <RoomIcon />
+              </Grid>
+              <Grid item>
+                <Typography>{city_id}</Typography>
+              </Grid>
+            </Grid>
           </Grid>
 
           <Grid item xs={12}>
-            <Typography variant='subtitle1'>Description:</Typography>
-            <Typography>{description}</Typography>
+            <Typography variant='h6'>Description</Typography>
+            <Typography variant='body1'>{description}</Typography>
           </Grid>
         </Grid>
       </Grid>
