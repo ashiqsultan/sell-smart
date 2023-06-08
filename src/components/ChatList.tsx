8ea2023d08c73daa64313bce6b1f207b01d90219
ashiqsultan <ashiqdrive@gmail.com>
2023-06-09 01:39:03 +0530

    Added loading modal to chat list

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -3,18 +3,23 @@ import { IUserChats, getUserChats } from '../api/chats';
 import { AppContext } from '../context/AppContext';
 import { useNavigate } from 'react-router-dom';
 import { Card, CardContent, List, ListItem, Typography } from '@mui/material';
+import ModalProgress from './ModalProgress';
 
 const ChatList = () => {
   const { state } = useContext(AppContext);
   const navigate = useNavigate();
   const [chatList, setChatList] = useState<IUserChats[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchChats = async () => {
+      setIsLoading(true);
       try {
         const userChats = await getUserChats(state.userId);
         setChatList(userChats);
+        setIsLoading(false);
       } catch (error) {
+        setIsLoading(false);
         console.error(error);
       }
     };
@@ -25,39 +30,40 @@ const ChatList = () => {
     navigate(`/chat/${chatId}`);
   };
 
-  if (chatList.length === 0) {
-    return (
-      <Typography variant='h6' component='div'>
-        No Chats Found
-      </Typography>
-    );
-  }
-
   return (
-    <List>
-      {chatList.map((chat) => (
-        <ListItem
-          key={chat.chatId}
-          onClick={() => handleCardClick(chat.chatId)}
-        >
-          <Card sx={{ width: '100%' }}>
-            <CardContent>
-              <Typography variant='h6' component='div'>
-                {chat.receiverName}
-              </Typography>
-              <Typography variant='body2' color='text.secondary'>
-                Last message:
-                {new Date(chat.updatedAt).toLocaleDateString('en-IN', {
-                  day: 'numeric',
-                  month: 'long',
-                  year: 'numeric',
-                })}
-              </Typography>
-            </CardContent>
-          </Card>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <ModalProgress isOpen={isLoading} />
+      {chatList.length === 0 ? (
+        <Typography variant='h6' component='div'>
+          No Chats Found
+        </Typography>
+      ) : (
+        <List>
+          {chatList.map((chat) => (
+            <ListItem
+              key={chat.chatId}
+              onClick={() => handleCardClick(chat.chatId)}
+            >
+              <Card sx={{ width: '100%' }}>
+                <CardContent>
+                  <Typography variant='h6' component='div'>
+                    {chat.receiverName}
+                  </Typography>
+                  <Typography variant='body2' color='text.secondary'>
+                    Last message:
+                    {new Date(chat.updatedAt).toLocaleDateString('en-IN', {
+                      day: 'numeric',
+                      month: 'long',
+                      year: 'numeric',
+                    })}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </>
   );
 };
 
