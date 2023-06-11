325135a6748903926fe5f145d574ae59f54f71d6
ashiqsultan <ashiqdrive@gmail.com>
2023-06-11 11:28:43 +0530

    Added margins

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import UserDetails from './pages/UserDetails';
 import ChatApp from './pages/ChatApp';
 import EditPost from './components/EditPost';
 import Chats from './pages/Chats';
+import { Box } from '@mui/material';
 
 const theme = createTheme({
   palette: {
@@ -22,25 +23,37 @@ const theme = createTheme({
 });
 
 function App() {
+  const appStyle = {
+    margin: '10px',
+    [theme.breakpoints.up('md')]: {
+      margin: '20px 50px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      margin: '24px 128px',
+    },
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <AppContextProvider>
           <AppBar />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/create-post' element={<CreatePost />} />
-            <Route path='/posts' element={<PostList />} />
-            <Route path='/post/:postId' element={<PostDetails />} />
-            <Route path='/post/edit/:postId' element={<EditPost />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/user/:userId' element={<UserDetails />} />
-            <Route path='/chat/:chatId' element={<ChatApp />} />
-            <Route path='/chats' element={<Chats />} />
+          <Box sx={appStyle}>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/create-post' element={<CreatePost />} />
+              <Route path='/posts' element={<PostList />} />
+              <Route path='/post/:postId' element={<PostDetails />} />
+              <Route path='/post/edit/:postId' element={<EditPost />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/user/:userId' element={<UserDetails />} />
+              <Route path='/chat/:chatId' element={<ChatApp />} />
+              <Route path='/chats' element={<Chats />} />
 
-            <Route path='*' element={<div>404 Page Not Found</div>} />
-          </Routes>
+              <Route path='*' element={<div>404 Page Not Found</div>} />
+            </Routes>
+          </Box>
         </AppContextProvider>
       </Router>
     </ThemeProvider>
