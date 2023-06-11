f8eaed706061435a7a57393cd1674e77fe4948ac
ashiqsultan <ashiqdrive@gmail.com>
2023-06-11 14:11:08 +0530

    Redesigned Signup page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,11 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import {
   TextField,
   Button,
-  Grid,
   Typography,
   Snackbar,
   Alert,
+  Card,
+  CardContent,
+  Box,
+  Avatar,
 } from '@mui/material';
+import { Person } from '@mui/icons-material';
 
 import { createAccount } from '../api/account';
 import { createUserDetail } from '../api/userDetails';
@@ -93,100 +97,112 @@ const SignUp: React.FC = () => {
   return (
     <>
       <ModalProgress isOpen={isLoading} />
-      <Grid container direction='column' alignItems='center' spacing={1}>
-        <Grid item>
-          <Typography variant='h5'>Sign Up</Typography>
-        </Grid>
-        <Grid item>
-          <form onSubmit={handleSubmit}>
-            <Grid container direction='column' spacing={2}>
-              <Grid item>
-                <TextField
-                  type='email'
-                  label='Email'
-                  value={email}
-                  onChange={handleEmailChange}
-                  variant='outlined'
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  type='password'
-                  label='Password'
-                  value={password}
-                  onChange={handlePasswordChange}
-                  variant='outlined'
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  type='text'
-                  label='Name'
-                  value={name}
-                  onChange={handleNameChange}
-                  variant='outlined'
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  type='text'
-                  label='Bio'
-                  value={bio}
-                  onChange={handleBioChange}
-                  variant='outlined'
-                  fullWidth
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  type='text'
-                  label='Phone Number'
-                  value={phoneNumber}
-                  onChange={handlePhoneNumberChange}
-                  variant='outlined'
-                  fullWidth
-                />
-              </Grid>
-              <Grid item alignContent={'center'} justifyContent={'center'}>
-                <Button
-                  type='submit'
-                  variant='contained'
-                  color='primary'
-                  disabled={isLoading}
-                >
-                  Sign Up
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
-        </Grid>
-        <Snackbar
-          open={!!error}
-          autoHideDuration={6000}
-          onClose={handleCloseSnackbar}
-        >
-          <Alert severity='error' onClose={handleCloseSnackbar}>
-            {error}
-          </Alert>
-        </Snackbar>
-        <Snackbar
-          open={!!successMessage}
-          autoHideDuration={6000}
-          onClose={handleCloseSnackbar}
-          sx={{ width: '50%' }}
-          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        >
-          <Alert severity='success' onClose={handleCloseSnackbar}>
-            {successMessage}
-          </Alert>
-        </Snackbar>
-      </Grid>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          height: '100vh',
+        }}
+      >
+        <Card>
+          <CardContent sx={{ padding: '2rem' }}>
+            <Box
+              display={'flex'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              flexDirection={'column'}
+              rowGap={1}
+            >
+              <Avatar>
+                <Person />
+              </Avatar>
+              <Typography variant='h5' component='div' sx={{ mb: 2 }}>
+                Sign up
+              </Typography>
+            </Box>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                type='email'
+                label='Email'
+                value={email}
+                onChange={handleEmailChange}
+                variant='outlined'
+                fullWidth
+                required
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                type='password'
+                label='Password'
+                value={password}
+                onChange={handlePasswordChange}
+                variant='outlined'
+                fullWidth
+                required
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                type='text'
+                label='Name'
+                value={name}
+                onChange={handleNameChange}
+                variant='outlined'
+                fullWidth
+                required
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                type='text'
+                label='Bio'
+                value={bio}
+                onChange={handleBioChange}
+                variant='outlined'
+                fullWidth
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                type='text'
+                label='Phone Number'
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+                variant='outlined'
+                fullWidth
+                sx={{ mb: 2 }}
+              />
+              <Button
+                type='submit'
+                variant='contained'
+                color='primary'
+                disabled={isLoading}
+                fullWidth
+              >
+                Sign Up
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Box>
+      <Snackbar
+        open={!!error}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert severity='error' onClose={handleCloseSnackbar}>
+          {error}
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={!!successMessage}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        sx={{ width: '50%' }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert severity='success' onClose={handleCloseSnackbar}>
+          {successMessage}
+        </Alert>
+      </Snackbar>
     </>
   );
 };
