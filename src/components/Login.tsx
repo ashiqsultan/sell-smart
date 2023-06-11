76a133c3691aa8ac2eaf805d3b7469354b602b81
ashiqsultan <ashiqdrive@gmail.com>
2023-06-11 14:03:45 +0530

    Redesigned login button

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,7 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import { login } from '../api/account';
 import { useContext, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
-
+import { Avatar, Card, CardContent, Typography } from '@mui/material';
+import { Lock } from '@mui/icons-material';
 const Login = () => {
   const navigate = useNavigate();
   const { state, dispatch } = useContext(AppContext);
@@ -33,31 +34,65 @@ const Login = () => {
   }, [state.userId]);
 
   return (
-    <Box component='form' onSubmit={handleSubmit} noValidate>
-      <TextField
-        margin='normal'
-        required
-        fullWidth
-        id='email'
-        label='Email Address'
-        name='email'
-        autoComplete='email'
-        autoFocus
-      />
-      <TextField
-        margin='normal'
-        required
-        fullWidth
-        name='password'
-        label='Password'
-        type='password'
-        id='password'
-        autoComplete='current-password'
-      />
-      <Button type='submit' fullWidth variant='contained' sx={{ mt: 2, mb: 2 }}>
-        Sign In
-      </Button>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        height: '100vh',
+      }}
+    >
+      <Card>
+        <CardContent sx={{ padding: '2rem' }}>
+          <Box
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            flexDirection={'column'}
+            rowGap={1}
+          >
+            <Avatar>
+              <Lock />
+            </Avatar>
+            <Typography variant='h5' component='div' sx={{ mb: 2 }}>
+              Login
+            </Typography>
+          </Box>
+
+          <Box component='form' onSubmit={handleSubmit} noValidate>
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 2, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
+
 export default Login;
